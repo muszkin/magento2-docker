9268b0473767073a5bcd6c16173f1fd6285328f8
pmucha <p.mucha@edrone.me>
2021-11-06 19:44:43 +0100

    init commit

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-[ ! -d ./php/src ] && composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition ./php/src
+[ ! -d ./php/src ] && composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition ./php/src \ 
+    && cp $HOME/.composer/auth.json ./php/auth.json
 [ ! -f ./nginx/nginx.conf.sample ] && cp ./php/src/nginx.conf.sample nginx/nginx.conf.sample
 docker-compose up -d
