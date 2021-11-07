137d7cd45be48cac78913cf2040e7a364c69f6c0
Piotr Mucha <p.mucha@edrone.me>
2021-11-07 13:31:20 +0100

    clean up

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[[ $OSTYPE != 'darwin'* ]] && sudo apt install php7.php7 4.4-common php7.4-gmp php7.4-curl php7.4-soap php7.4-bcmath php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-mcrypt php7.4-mysql php7.4-gd php7.4-xml php7.4-cli php7.4-zip
 [ ! -d ./php/src ] && composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition ./php/src
 [ ! -f ./php/auth.json ] && cp $HOME/.composer/auth.json ./php
 [ ! -f ./nginx/nginx.conf.sample ] && cp ./php/src/nginx.conf.sample nginx/nginx.conf.sample
